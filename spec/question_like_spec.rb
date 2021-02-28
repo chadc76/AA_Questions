8726ad2f1f4be1e53c6c1d24007dfca87ff4bb6a
Chad Cunningham <chadcunningham7676@gmail.com>
2021-02-28 06:14:23 -0500

    Completed question_like spec

--- spec/question_like_spec.rb
+++ spec/question_like_spec.rb
@@ -6,4 +6,70 @@ describe QuestionLike do
   before(:each) { QuestionDatabase.reset! }
   after(:each) { QuestionDatabase.reset! }
   
+  describe 'Question::likers_for_question_id' do
+    subject(:likers) { described_class.likers_for_question_id(4) }
+
+    it 'only hits the database once' do 
+      expect(QuestionDatabase).to receive(:execute).exactly(1).times.and_call_original
+      described_class.likers_for_question_id(1)
+    end
+
+    it 'returns an array of instances of user class' do
+      expect(likers).to all( be_an(User) )
+    end
+
+    it 'returns correct likers for the question' do 
+      expect(likers.map(&:id)).to match_array([2, 3, 4, 5, 6])
+    end
+  end
+
+  describe 'Question::num_likes_for_question_id' do
+    subject(:num_likes) { described_class.num_likes_for_question_id(4) }
+
+    it 'only hits the database once' do 
+      expect(QuestionDatabase).to receive(:get_first_value).exactly(1).times.and_call_original
+      described_class.num_likes_for_question_id(1)
+    end
+
+    it 'returns correct number of likes for the question' do
+      expect(num_likes).to eq(5)
+    end
+  end
+
+  describe 'Question::liked_questions_for_user_id' do
+    subject(:questions) { described_class.liked_questions_for_user_id(2) }
+
+    it 'only hits the database once' do 
+      expect(QuestionDatabase).to receive(:execute).exactly(1).times.and_call_original
+      described_class.liked_questions_for_user_id(1)
+    end
+
+    it 'returns an array of instances of the question class' do
+      expect(questions).to all( be_an(Question) )
+    end
+
+    it 'returns correct questions liked by the user' do
+      expect(questions.map(&:id)).to match_array([1, 3, 4])
+    end
+  end
+
+  describe 'Question::most_liked_questions' do
+    subject(:most_liked) { described_class.most_liked_questions(1) }
+    let(:top_three) { described_class.most_liked_questions(3) }
+
+    it 'only hits the database once' do 
+      expect(QuestionDatabase).to receive(:execute).exactly(1).times.and_call_original
+      described_class.most_liked_questions(1)
+    end
+
+    it 'returns an array of instances of the question class' do
+      expect(most_liked).to all( be_an(Question) )
+    end
+
+    it 'returns correct questions liked by the user' do
+      expect(most_liked.map(&:id)).to match_array([4])
+      expect(top_three.map(&:id)).to match_array([4, 3, 2])
+    end
+  end
+
 end 
